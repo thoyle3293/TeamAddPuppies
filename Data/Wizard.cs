38ea2fe30e73cb8d52adbab8be6ab51c53b5a0e0
aja025 <andrew.antila2@gmail.com>
2017-09-11 16:13:51 -0700

    aa Wizards

---
+++ Data/Wizard.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CSDay01.Data
+{
+
+    public class Wizard
+    {   
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Level { get; set; }
+        public string Skills {get; set;}
+        public int Age { get; set; }
+        public Dragon SpiritAnimal 
+        public double Height { get; set; }
+    }
+}
